bf9f891402ee0786d9e4beeb57ea88d7291137ee
MerrillAchigan <87760869+MerrillAchigan@users.noreply.github.com>
2024-12-05 22:48:20 +0100

    basic setup

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const weatherForm = document.querySelector('.weatherForm');
+const cityInput = document.querySelector('.cityInput');
+const card = document.querySelector('.card');
+
+const apikey = 'a22973cfff3b959eeb60f875615b8653';
