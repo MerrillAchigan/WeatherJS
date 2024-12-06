f4aa59bacb0bb87349ba7bd56ad3e2fb94d2c3d6
MerrillAchigan <87760869+MerrillAchigan@users.noreply.github.com>
2024-12-06 19:50:57 +0100

    display things

--- js/index.js
+++ js/index.js
@@ -3,3 +3,38 @@ const cityInput = document.querySelector('.cityInput');
 const card = document.querySelector('.card');
 
 const apikey = 'a22973cfff3b959eeb60f875615b8653';
+
+weatherForm.addEventListener((submit), event => {
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+
+    } else{
+        displayError('goddamn dumbahh enter a City')
+    }
+});
+
+async function getWeather(city){
+
+}
+
+function displayWeather(data){
+
+}
+
+function getEmoji(weatherId){
+
+}
+
+function displayError(message){
+    const errorDisplay = document.createElement('p');
+    errorDisplay.textContent = message;
+
+    errorDisplay.classList.add('errorDisplay');
+
+    card.textContent = '';
+    card.computedStyleMap.display = 'flex';
+    card.appendChild(errorDisplay);
+}
